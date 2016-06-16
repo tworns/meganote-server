e45c9b65263602fa154fff1f639eaf29379b104f
tworns <tworns@yahoo.com>
2016-06-16 10:43:12 -0400

    Added basic get function

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ var express = require('express');
 var app = express();
 
 app.listen('3030', function(){console.log('listening on http localhost port 3030');});
+app.get('/',function(req,res){
+  res.send('I <3 pizza');
+
+});
