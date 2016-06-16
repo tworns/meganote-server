e4d0b0835cd375eaa334ae3946117dcf1bfd72a6
tworns <tworns@yahoo.com>
2016-06-16 11:33:46 -0400

    Added environment variables files and dotenv, updated gitignore, added mongoose

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+require('dotenv').load(); //requires environment variables
 var express = require('express');
+var db = require('mongoose');
 var app = express();
 
+db.connect();
 app.listen('3030', function(){console.log('listening on http localhost port 3030');});
+var note = db.model('Note', {title:String});
 app.get('/',function(req,res){
-  res.json([
-    {title: 'hardcoded note', body_html: 'Cool kids make notes'},
-    {title: 'also hard coded', body_html : 'Node does stuff'},
-  ]);
+  Note.find().then(function(notes){
+    res.json(notes);
+  });
 
 });
