e6a11ea442209275578f0b187bd0d21780ba73dd
tworns <tworns@yahoo.com>
2016-06-16 12:00:41 -0400

    Added middleware to inject CORS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').load(); //requires environment variables
+require('dotenv').load(); //requires environment variables and loads them in
 var express = require('express');
 var app = express();
 var db = require('./config/db');
@@ -6,7 +6,10 @@ var Note = require('./models/note');
 app.listen('3030', function(){
   console.log('listening on http localhost port 3030');
 });
-
+app.use(function(req,res,next){
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 var Note = db.model('Note', {title:String});
 app.get('/',function(req,res){
   Note.find({}, {title:1})
