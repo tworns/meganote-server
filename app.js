6dc1b7b8a6f2dbce7acdc95893dd93d5e3edfccf
tworns <tworns@yahoo.com>
2016-06-16 10:53:02 -0400

    Added supervisor and hardcoded test json

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.listen('3030', function(){console.log('listening on http localhost port 3030');});
 app.get('/',function(req,res){
-  res.send('I <3 pizza');
+  res.json([
+    {title: 'hardcoded note', body_html: 'Cool kids make notes'},
+    {title: 'also hard coded', body_html : 'Node does stuff'},
+  ]);
 
 });
