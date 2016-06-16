a72b6e0293c646d2b20715e2dd40111424ad2380
tworns <tworns@yahoo.com>
2016-06-16 11:39:56 -0400

    Added NoteSchema

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ var db = require('mongoose');
 var app = express();
 
 db.connect();
-app.listen('3030', function(){console.log('listening on http localhost port 3030');});
-var note = db.model('Note', {title:String});
+app.listen('3030', function(){
+  console.log('listening on http localhost port 3030');
+});
+var NoteSchema = db.Schema({
+  title:String,
+  body_html:String,
+  body_text:String,
+  updated_at:{type:date,default:Date.now}
+});
+var Note = db.model('Note', {title:String});
 app.get('/',function(req,res){
-  Note.find().then(function(notes){
+  Note.find({}, {title:1})
+  .then(function(notes){
     res.json(notes);
   });
 
