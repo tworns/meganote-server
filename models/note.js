477bb65cd2c0e294816ca74d2e5c1d75c812bfdc
tworns <tworns@yahoo.com>
2016-06-17 10:54:41 -0400

    Sanitized html and saved html as text

--- models/note.js
+++ models/note.js
@@ -1,4 +1,6 @@
 var db = require('../config/db');
+var sanitizeHtml = require('sanitize-html');
+var htmlToText = require('html-to-text');
 var NoteSchema = db.Schema({
   title:String,
   body_html:String,
@@ -7,6 +9,8 @@ var NoteSchema = db.Schema({
 });
 NoteSchema.pre('save',function(next){
   this.updated_at = Date.now();
+  this.body_text = htmlToText.fromString(this.body_html);
+  this.body_html = sanitizeHtml(this.body_html);
   next();
 });
 module.exports = Note;
