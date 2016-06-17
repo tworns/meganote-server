cf94f3a661f5a0d85462fc23262810ea64ee4e83
tworns <tworns@yahoo.com>
2016-06-17 09:44:21 -0400

    Added post handling and a readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Meganote Server
+
+Meganote server is a REST API for note-taking.
+
+## Setup
+
+Copy and edit DOTENV configuration
+
+```shell
+cp .env.example .env
+```
+Edit the new `.env` file and fill in the variables with appropriate values.
+
+## Starting the server
+
+In development, run `npm run dev` to start the supervisor.
+
+In production, run `npm start`.
--- app.js
+++ app.js
@@ -3,18 +3,38 @@ var express = require('express');
 var app = express();
 var db = require('./config/db');
 var Note = require('./models/note');
+var bodyParser = require('body-parser');
+
 app.listen('3030', function(){
   console.log('listening on http localhost port 3030');
 });
+
 app.use(function(req,res,next){
   res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers','Content-Type');
   next();
 });
+app.use(bodyParser.json());
+
 var Note = db.model('Note', {title:String});
 app.get('/',function(req,res){
-  Note.find({}, {title:1})
+  Note.find({}, {title:'1'})
   .then(function(notes){
     res.json(notes);
+  });
+  app.post('/',function(req,res){
+      var note = new Note({
+        title: req.body.note.title,
+        body_html: req.body.note.body_html
+      });
+
+      note.save()
+      .then(function(noteData){
+        res.json({
+          message: 'successfully updated note',
+          note:noteData
+        });
+      });
   });
 
 });
